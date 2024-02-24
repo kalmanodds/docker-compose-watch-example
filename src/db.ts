838d5d77d57b6bfcc9a22897065339dfe85045f8
kalmanodds <kalman@tix.is>
2024-02-24 17:22:54 +0100

    Data fetching for page.tsx

---
+++ src/db.ts
@@ -0,0 +1,20 @@
+import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+// Fix for "sorry, too many clients already"
+declare global {
+  // eslint-disable-next-line no-var -- only var works here
+  var db: PostgresJsDatabase | undefined;
+}
+
+let db: PostgresJsDatabase;
+
+if (process.env.NODE_ENV === "production") {
+  db = drizzle(postgres(process.env.POSTGRES_URL!));
+} else {
+  if (!global.db) global.db = drizzle(postgres(process.env.POSTGRES_URL!));
+
+  db = global.db;
+}
+
+export { db };
